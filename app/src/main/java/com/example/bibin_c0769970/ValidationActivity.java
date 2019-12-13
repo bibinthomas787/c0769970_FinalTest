17eda9d7c65ad1869ca7f5be8014dca962771938
Bibin <bibinthomas787@gmail.com>
2019-12-13 18:10:07 -0500

    committed

--- app/src/main/java/com/example/bibin_c0769970/ValidationActivity.java
+++ app/src/main/java/com/example/bibin_c0769970/ValidationActivity.java
@@ -6,16 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class ValidationActivity extends AppCompatActivity {
     GridView gridView;
+    public String someVariable;
 
     int[] imageIcons = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4,
             R.drawable.img8, R.drawable.img9};
     String[] numbers = {
-            "one", "two", "three",
-            "four", "five", "six",
+            "img1", "img2", "img3",
+            "img4", "img8", "img9",
 
     };
 
@@ -34,7 +36,11 @@ public class ValidationActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                ImageView imageView = (ImageView) view;
+                imageView.setImageResource(R.drawable.checked);
 
+
+               // imageView.setBackgroundResource(R.drawable.iconAdapter.getItem(position));
 
                  Toast.makeText(ValidationActivity.this, "You clicked on " + iconAdapter.getItem(position), Toast.LENGTH_SHORT).show();
             }
